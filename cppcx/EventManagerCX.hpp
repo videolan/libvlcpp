f4acbed9433cb825fdec64201f9146306e735fe0
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2016-05-06 16:59:36 +0200

    EventManagerCX: Fix potential double inclusion issue

--- cppcx/EventManagerCX.hpp
+++ cppcx/EventManagerCX.hpp
@@ -20,6 +20,8 @@
 * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
 *****************************************************************************/
 
+#pragma once
+
 #include <vlcpp/vlc.hpp>
 #include "StructuresCX.hpp"
 #include "MediaCX.hpp"
